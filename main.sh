294da4d7fc52487f0b9d3c555e71ff912309bea5
Paul Tran <paul.tran@sap.com>
2018-02-15 18:14:03 -0800

    read from file for files to grep against

--- main.sh
+++ main.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-set -f
+# messes up path specified to grep (possibly due to alias)
+# set -f
 
 styles_file=$1
 target_file=$2
 
 output_dir=temp
-output_pos_file=$output_dir/output_positive.txt
-output_neg_file=$output_dir/output_negative.txt
+output_pos_file=$output_dir/output_positive.css
+output_neg_file=$output_dir/output_negative.css
 
 if [[ ! -f $styles_file ]]; then
     echo "Styles file was not specified properly"
@@ -29,19 +30,27 @@ if [[ -f $output_neg_file ]]; then
     rm $output_neg_file
 fi
 
-selector_regex="^\.([^\ ]*)[,\ ]"
+selector_regex="^\.([^,:>\ ]*)[,:>\ ]"
 while read line; do
     # selector does not update unless a positive match occurs
     [[ "$line" =~ $selector_regex ]] && selector=${BASH_REMATCH[1]}
 
-    if grep -q "$selector" "$target_file"; then
+    grep_match_found=1
+    while read file; do
+        if grep -q "$selector" "$file"; then
+            grep_match_found=0
+        fi
+    done < $target_file
+
+    if [[ $grep_match_found -eq 0 ]]; then
     	echo $line >> $output_pos_file
-	echo >> $output_neg_file
+        echo >> $output_neg_file
     else
-	echo >> $output_pos_file
+        echo >> $output_pos_file
     	echo $line >> $output_neg_file
     fi
-done < $1
+
+done < $styles_file
 
 # internal field seperator, change new lines delimter
 # IFS=
